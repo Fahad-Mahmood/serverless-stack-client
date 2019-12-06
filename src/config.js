881556e4c0531ab6054e6e12f289ab35a0374761
Fahad Mahmood <fahad492@outlook.com>
2019-12-06 11:29:03 +0500

    Setting up our React App

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-west-2,
+    BUCKET: "notes-app-f"
+  },
+  apiGateway: {
+    REGION: "us-west-2",
+    URL: "https://habvnvb3ri.execute-api.us-west-2.amazonaws.com/prod/"
+  },
+  cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_vNsgeQ0jv",
+    APP_CLIENT_ID: "6c4kgb7fe7kk838avaha2rp19f",
+    IDENTITY_POOL_ID: "us-west-2:edbce6f7-795c-47f9-a826-aa391a456083"
+  }
+};
